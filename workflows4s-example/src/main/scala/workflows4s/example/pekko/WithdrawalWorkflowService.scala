1ec242ce443409d25fb5b1230bb03233e707041f
lmlynik <lukasz.mlynik@hotmail.com>
2026-01-01 15:46:23 +0100

    Revert "remove duplication"
    This reverts commit 9123f635dcc5fc40dee7c76c23b92955e2d1e3da.

--- workflows4s-example/src/main/scala/workflows4s/example/pekko/WithdrawalWorkflowService.scala
+++ workflows4s-example/src/main/scala/workflows4s/example/pekko/WithdrawalWorkflowService.scala
@@ -1,59 +1,53 @@
 package workflows4s.example.pekko
 
+import cats.effect.IO
 import org.apache.pekko.actor.typed.ActorSystem
 import org.apache.pekko.persistence.query.scaladsl.{CurrentPersistenceIdsQuery, ReadJournal}
 import org.apache.pekko.stream.scaladsl.Sink
-import workflows4s.example.withdrawal.{FutureWithdrawalWorkflow, WithdrawalData, WithdrawalSignal, WithdrawalWorkflow}
 import workflows4s.example.withdrawal.WithdrawalSignal.CreateWithdrawal
+import workflows4s.example.withdrawal.{WithdrawalData, WithdrawalSignal, WithdrawalWorkflow}
 import workflows4s.runtime.WorkflowInstance.UnexpectedSignal
 import workflows4s.runtime.pekko.PekkoRuntime
 
-import scala.concurrent.{ExecutionContext, Future}
-
-/** Future-based version of WithdrawalWorkflowService for use with FutureEffect.
-  */
 trait WithdrawalWorkflowService {
 
-  def startWorkflow(id: String, input: WithdrawalSignal.CreateWithdrawal): Future[Unit]
+  def startWorkflow(id: String, input: WithdrawalSignal.CreateWithdrawal): IO[Unit]
 
-  def listWorkflows: Future[Seq[String]]
+  def listWorkflows: IO[Seq[String]]
 
-  def getState(id: String): Future[WithdrawalData]
+  def getState(id: String): IO[WithdrawalData]
 
-  def cancelWithdrawal(id: String, request: WithdrawalSignal.CancelWithdrawal): Future[Unit]
+  def cancelWithdrawal(id: String, request: WithdrawalSignal.CancelWithdrawal): Unit
 
-  def markAsExecuted(id: String, request: WithdrawalSignal.ExecutionCompleted): Future[Unit]
+  def markAsExecuted(id: String, request: WithdrawalSignal.ExecutionCompleted): Unit
 
 }
 
-object FutureWithdrawalWorkflowService {
+object WithdrawalWorkflowService {
   type Journal = ReadJournal & CurrentPersistenceIdsQuery
 
-  class Impl(journal: Journal, wdRuntime: PekkoRuntime[WithdrawalWorkflow.Context.Ctx])(using
-      val actorSystem: ActorSystem[Any],
-      ec: ExecutionContext,
-  ) extends WithdrawalWorkflowService {
+  class Impl(journal: Journal, wdRuntime: PekkoRuntime[WithdrawalWorkflow.Context.Ctx])(using val actorSystem: ActorSystem[Any])
+      extends WithdrawalWorkflowService {
 
-    override def startWorkflow(id: String, input: CreateWithdrawal): Future[Unit] = {
+    override def startWorkflow(id: String, input: CreateWithdrawal): IO[Unit] = {
       val workflow = wdRuntime.createInstance_(id)
-      workflow
-        .deliverSignal(WithdrawalWorkflow.Signals.createWithdrawal, input)
+      IO.fromFuture(IO(workflow.deliverSignal(WithdrawalWorkflow.Signals.createWithdrawal, input)))
         .map({
           case Right(response)             => response
           case Left(UnexpectedSignal(sig)) => throw new Exception(s"Unexpected creation signal $sig for instance ${id}")
         })
     }
 
-    override def listWorkflows: Future[Seq[String]] = journal.currentPersistenceIds().runWith(Sink.seq)
+    override def listWorkflows: IO[Seq[String]] = IO.fromFuture(IO(journal.currentPersistenceIds().runWith(Sink.seq)))
 
-    override def getState(id: String): Future[WithdrawalData] = {
+    override def getState(id: String): IO[WithdrawalData] = {
       val workflow = wdRuntime.createInstance_(id)
-      workflow.queryState()
+      IO.fromFuture(IO(workflow.queryState()))
     }
 
-    override def cancelWithdrawal(id: String, request: WithdrawalSignal.CancelWithdrawal): Future[Unit] = ???
+    override def cancelWithdrawal(id: String, request: WithdrawalSignal.CancelWithdrawal): Unit = ???
 
-    override def markAsExecuted(id: String, request: WithdrawalSignal.ExecutionCompleted): Future[Unit] = ???
+    override def markAsExecuted(id: String, request: WithdrawalSignal.ExecutionCompleted): Unit = ???
   }
 
 }
